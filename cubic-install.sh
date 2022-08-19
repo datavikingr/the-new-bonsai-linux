380f0ac78d4da3cde92250b1d3dac6a32b9c33f4
Alex Haskins <alexanderwantshislibraryback@gmail.com>
2022-08-18 22:10:45 -0400

    Added some rolling rhino for semi-rolling release action

--- cubic-install.sh
+++ cubic-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # cubic-install.sh - Written by Alex Haskins
+# considers ubuntu-22.04-live-server-amd64.iso as base
 
 #init
 repofolder=$(pwd)
@@ -33,6 +34,7 @@ apt install sxhkd polybar rofi picom feh pulseaudio dunst xterm kitty thunar pyt
 apt install sddm qml-module-qtquick-layouts qml-module-qtquick-controls2 qml-module-qtquick-templates2 qml-module-qtgraphicaleffects -y
 cp .config/sddm/etc-sddm.conf /etc/sddm.conf
 cp -r .config/sddm/mount/* /usr/share/sddm/themes/mount/
+systemctl enable sddm.service
 
 #cli system tools
 apt install zsh micro ranger docker nala htop bmon -y
@@ -127,6 +129,63 @@ cp .fehbg /etc/skel
 rm -rf .config/sddm/
 cp -r .config/ /etc/skel/
 cp -r .local/bin/ /etc/skel/
+
+#adding some rolling rhino to the mix
+OS_CODENAME=$(lsb_release --codename --short)
+HOST_ARCH="$(uname --machine)"
+if [ "${HOST_ARCH}" == "x86_64" ]; then
+	ARCHIVE="http://archive.ubuntu.com/ubuntu"
+	SEC_ARCHIVE="http://security.ubuntu.com/ubuntu"
+else
+	ARCHIVE="http://ports.ubuntu.com/ubuntu-ports"
+	SEC_ARCHIVE="http://ports.ubuntu.com/ubuntu-ports"
+fi
+cp  /etc/apt/sources.list /etc/apt/sources.list.${OS_CODENAME}
+cat << EOF > /etc/apt/sources.list
+# See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
+# newer versions of the distribution.
+deb ${ARCHIVE} devel main restricted
+# deb-src ${ARCHIVE} devel main restricted
+## Major bug fix updates produced after the final release of the
+## distribution.
+deb ${ARCHIVE} devel-updates main restricted
+# deb-src ${ARCHIVE} devel-updates main restricted
+## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
+## team. Also, please note that software in universe WILL NOT receive any
+## review or updates from the Ubuntu security team.
+deb ${ARCHIVE} devel universe
+# deb-src ${ARCHIVE} devel universe
+deb ${ARCHIVE} devel-updates universe
+# deb-src ${ARCHIVE} devel-updates universe
+## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
+## team, and may not be under a free licence. Please satisfy yourself as to
+## your rights to use the software. Also, please note that software in
+## multiverse WILL NOT receive any review or updates from the Ubuntu
+## security team.
+deb ${ARCHIVE} devel multiverse
+# deb-src ${ARCHIVE} devel multiverse
+deb ${ARCHIVE} devel-updates multiverse
+# deb-src ${ARCHIVE} devel-updates multiverse
+## N.B. software from this repository may not have been tested as
+## extensively as that contained in the main release, although it includes
+## newer versions of some applications which may provide useful features.
+## Also, please note that software in backports WILL NOT receive any review
+## or updates from the Ubuntu security team.
+deb ${ARCHIVE} devel-backports main restricted universe multiverse
+# deb-src ${ARCHIVE} devel-backports main restricted universe multiverse
+## Uncomment the following two lines to add software from Canonical's
+## 'partner' repository.
+## This software is not part of Ubuntu, but is offered by Canonical and the
+## respective vendors as a service to Ubuntu users.
+# deb http://archive.canonical.com/ubuntu devel partner
+# deb-src http://archive.canonical.com/ubuntu devel partner
+deb ${SEC_ARCHIVE} devel-security main restricted
+# deb-src ${SEC_ARCHIVE} devel-security main restricted
+deb ${SEC_ARCHIVE} devel-security universe
+# deb-src ${SEC_ARCHIVE} devel-security universe
+deb ${SEC_ARCHIVE} devel-security multiverse
+# deb-src ${SEC_ARCHIVE} devel-security multiverse
+EOF
 
 #cleanup
 cd ..
