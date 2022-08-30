c7b034473f98d498a9aa6ec748030cd3851c97e9
Alex Haskins <alexanderwantshislibraryback@gmail.com>
2022-08-30 16:11:51 -0400

    prepping for polybar handling multiple resolutions

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+# Screen resolution. Currently report 2560x1440, so we'd see 1920x1080 for that monitor.
+# xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/'
+
 # Launch
 polybar left &
 polybar center &
