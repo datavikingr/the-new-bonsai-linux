9b7b977a83eacf21c8593af9f3fbbf3e6443b2dc
Alex Haskins <alexanderwantshislibraryback@gmail.com>
2022-09-01 12:02:42 -0400

    added 1080p support, tweaked colors for more pop

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,11 +5,14 @@ killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
+# gather screen resolution information
+screenres=$(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/')
 
-# Screen resolution. Currently report 2560x1440, so we'd see 1920x1080 for that monitor.
-# xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/'
-
-# Launch
-polybar left &
-polybar center &
-polybar right &
+if [ $screenres == '2560x1440' ] ; then
+	# Launch the triple bar
+	polybar left &
+	polybar center &
+	polybar right &
+else
+	polybar mono &
+fi
