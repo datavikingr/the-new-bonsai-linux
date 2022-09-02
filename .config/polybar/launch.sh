da8f05634541300a7848a8b235c25815bc5e5558
Alex Haskins <alexanderwantshislibraryback@gmail.com>
2022-09-02 16:45:24 -0400

    overhauled and simplified to the better-designed bar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,14 +5,5 @@ killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
-# gather screen resolution information
-screenres=$(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/')
 
-if [ $screenres == '2560x1440' ] ; then
-	# Launch the triple bar
-	polybar left &
-	polybar center &
-	polybar right &
-else
-	polybar mono &
-fi
+polybar mono &
