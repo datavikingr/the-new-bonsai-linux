efc0d372b6155559b0fc64f16e96468919468721
Alex Haskins <alexanderwantshislibraryback@gmail.com>
2022-09-12 09:07:41 -0400

    removed rolling rhino. added godot. add distrobox setup.

--- cubic-install.sh
+++ cubic-install.sh
@@ -28,7 +28,8 @@ dpkg -i berry_0.1.11-1_amd64_bullseye.deb
 rm berry_0.1.11-1_amd64_bullseye.deb
 rm -rf /usr/share/xsessions/
 mv .config/berry/berry.desktop /usr/share/xsessions/
-apt install sxhkd polybar rofi picom feh pulseaudio dunst xterm kitty thunar python3 python3-pip -y
+apt install sxhkd polybar rofi picom feh pulseaudio dunst xterm kitty thunar python3 python3-pip lxappearance -y
+apt install lxsession --no-install-recommends
 
 #sddm and dependencies
 apt install sddm qml-module-qtquick-layouts qml-module-qtquick-controls2 qml-module-qtquick-templates2 qml-module-qtgraphicaleffects -y
@@ -39,7 +40,7 @@ systemctl enable sddm.service
 #cli system tools
 echo "deb https://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
 wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
-apt install zsh micro ranger docker nala htop bmon -y
+apt install zsh micro ranger nala htop bmon unzip -y
 
 #cli fun
 apt install lolcat neofetch gnugo gnuchess cbonsai cmatrix fortune-mod nethack-console -y
@@ -53,7 +54,21 @@ add-apt-repository ppa:oguzhaninan/stacer -y
 add-apt-repository ppa:cappelikan/ppa
 apt-get update
 apt-get install notepadqq scrot timeshift gparted stacer mainline -y
+
+#distrobox and set up
+apt install docker docker-compose
 curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | /bin/bash
+distrobox create -i docker.io/library/archlinux:latest -n arch
+distrobox create -i docker.io/library/debian:testing-backports -n debian
+distrobox create -i quay.io/fedora/fedora:rawhide -n fedora
+
+#godot
+mkdir -p /etc/skel/.local/bin
+cd /etc/skel/.local/bin
+wget https://downloads.tuxfamily.org/godotengine/3.5/Godot_v3.5-stable_x11.64.zip
+cp Godot_v3.5-stable_x11.64.zip godot.zip
+rm Godot_v3.5-stable_x11.64.zip
+unzip godot.zip
 
 #homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -68,7 +83,7 @@ deb-get install brave-browser discord
 #powerline
 pip install powerline-shell
 
-#bashrc
+#bashrc - required for distrobox-branches's terminals
 touch /etc/skel/.bashrc
 cat << EOF > /etc/skel/.bashrc
 #local binaries
@@ -130,64 +145,7 @@ cd $repofolder
 cp .fehbg /etc/skel
 rm -rf .config/sddm/
 cp -r .config/ /etc/skel/
-cp -r .local/bin/ /etc/skel/
-
-#adding some rolling rhino to the mix
-OS_CODENAME=$(lsb_release --codename --short)
-HOST_ARCH="$(uname --machine)"
-if [ "${HOST_ARCH}" == "x86_64" ]; then
-	ARCHIVE="http://archive.ubuntu.com/ubuntu"
-	SEC_ARCHIVE="http://security.ubuntu.com/ubuntu"
-else
-	ARCHIVE="http://ports.ubuntu.com/ubuntu-ports"
-	SEC_ARCHIVE="http://ports.ubuntu.com/ubuntu-ports"
-fi
-cp  /etc/apt/sources.list /etc/apt/sources.list.${OS_CODENAME}
-cat << EOF > /etc/apt/sources.list
-# See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
-# newer versions of the distribution.
-deb ${ARCHIVE} devel main restricted
-# deb-src ${ARCHIVE} devel main restricted
-## Major bug fix updates produced after the final release of the
-## distribution.
-deb ${ARCHIVE} devel-updates main restricted
-# deb-src ${ARCHIVE} devel-updates main restricted
-## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
-## team. Also, please note that software in universe WILL NOT receive any
-## review or updates from the Ubuntu security team.
-deb ${ARCHIVE} devel universe
-# deb-src ${ARCHIVE} devel universe
-deb ${ARCHIVE} devel-updates universe
-# deb-src ${ARCHIVE} devel-updates universe
-## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
-## team, and may not be under a free licence. Please satisfy yourself as to
-## your rights to use the software. Also, please note that software in
-## multiverse WILL NOT receive any review or updates from the Ubuntu
-## security team.
-deb ${ARCHIVE} devel multiverse
-# deb-src ${ARCHIVE} devel multiverse
-deb ${ARCHIVE} devel-updates multiverse
-# deb-src ${ARCHIVE} devel-updates multiverse
-## N.B. software from this repository may not have been tested as
-## extensively as that contained in the main release, although it includes
-## newer versions of some applications which may provide useful features.
-## Also, please note that software in backports WILL NOT receive any review
-## or updates from the Ubuntu security team.
-deb ${ARCHIVE} devel-backports main restricted universe multiverse
-# deb-src ${ARCHIVE} devel-backports main restricted universe multiverse
-## Uncomment the following two lines to add software from Canonical's
-## 'partner' repository.
-## This software is not part of Ubuntu, but is offered by Canonical and the
-## respective vendors as a service to Ubuntu users.
-# deb http://archive.canonical.com/ubuntu devel partner
-# deb-src http://archive.canonical.com/ubuntu devel partner
-deb ${SEC_ARCHIVE} devel-security main restricted
-# deb-src ${SEC_ARCHIVE} devel-security main restricted
-deb ${SEC_ARCHIVE} devel-security universe
-# deb-src ${SEC_ARCHIVE} devel-security universe
-deb ${SEC_ARCHIVE} devel-security multiverse
-# deb-src ${SEC_ARCHIVE} devel-security multiverse
-EOF
+cp .local/bin/* /etc/skel/.local/bin/
 
 #cleanup
 cd ..
